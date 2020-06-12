f580fb01a755f1b41c9d8ffaa0c03b1b591a798d
Alexander GITTINGS <alexander.gittings@axians.co.uk>
2020-06-12 18:22:23 +0100

    📜 Update Poetry

---
+++ axians_netbox_pdu/__init__.py
@@ -0,0 +1,19 @@
+__version__ = "0.0.1"
+
+from extras.plugins import PluginConfig
+
+class PDUConfig(PluginConfig):
+    """Plugin configuration for the axians_netbox_pdu plugin."""
+
+    name = "axians_netbox_pdu"
+    verbose_name = "PDU Status"
+    version = __version__
+    author = "Alexander Gittings"
+    description = "A plugin for NetBox to easily get PDU information."
+    base_url = "pdu"
+    required_settings = []
+    min_version = "2.8.1"
+    default_settings = {}
+    caching_config = {}
+
+config = PDUConfig # pylint:disable=invalid-name
---
+++ development/netbox_master/configuration.py
@@ -0,0 +1,4 @@
+"""NetBox configuration file overrides specific to the latest MASTER version."""
+from .base_configuration import *  # pylint: disable=relative-beyond-top-level, wildcard-import
+
+# Overrides specific to this version go here
